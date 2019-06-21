d27ddbb9e76cc4cc9008ddf10d05e48583280ea0
krysttian <krysttian.duncan@gmail.com>
2019-06-21 15:27:15 -0400

    slightly better frontend

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 window.addEventListener("DOMContentLoaded",function(){
-    console.log('dom loaded')
     const textAreaSelector = () => document.getElementById('urlTextArea');
     const codeBoxSelector = () => document.getElementById('codebox');
-    textAreaSelector().addEventListener('keypress', (function() {
-        const textAreaValues = textAreaSelector().value.split('\n');
-        console.dir(textAreaValues);
-        codeBoxSelector().innerText = JSON.stringify(textAreaValues);
-    }))
+    // const copyClipboardSelector = () => document.getElementById('copyToClipBoard');
+
+    const uriString = 'https://2rygsn9pzc.execute-api.us-east-1.amazonaws.com/dev/amp?';
+
+    function checkAndGenerateUrl (){
+        const textAreaValues = textAreaSelector().value.trim().split('\n');
+        let queryString = '';
+        textAreaValues.forEach(element => {
+            element ? queryString += `url=${element}&` : undefined;
+        });
+        codeBoxSelector().innerText = uriString + queryString;
+    }
+
+    checkAndGenerateUrl();
+
+    textAreaSelector().addEventListener('input', (function() {
+        checkAndGenerateUrl();
+    }));
+    // TODO this is pretty tough without a library and without doing silly stuff
+    // copyClipboardSelector().addEventListener('click', (function() {
+    //     codeBoxSelector().select();
+    //     document.execCommand('copy');
+    // }))
 });
